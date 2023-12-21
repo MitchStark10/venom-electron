bccfc852d99322a29b4b67441369a697348681c2
Mitch Stark <mitchstark10@gmail.com>
2023-12-21 15:41:07 -0600

    add projects to sidebar

--- src/Layout/Sidebar/index.tsx
+++ src/Layout/Sidebar/index.tsx
@@ -1,5 +1,6 @@
-import { Task, Today, Upcoming } from "@mui/icons-material";
-import { styled } from "@mui/material";
+import { Task, TaskAlt, Today, Upcoming } from "@mui/icons-material";
+import { Divider, styled } from "@mui/material";
+import { projectList } from "../../mockData/projectList";
 import { SidebarMenuItem } from "./SidebarMenuItem";
 
 const SidebarContainer = styled("span")(({ theme }) => ({
@@ -13,13 +14,24 @@ const SidebarContainer = styled("span")(({ theme }) => ({
   boxShadow: `3px 0 5px -2px ${theme.palette.grey[300]}`,
 }));
 
+const DividerWithPadding = styled(Divider)(({ theme }) => ({
+  margin: `${theme.spacing(4)} 0 ${theme.spacing(8)} 0 !important`,
+}));
+
 export const SideBar = () => {
   return (
     <SidebarContainer>
       <SidebarMenuItem icon={<Today />} title="Today" />
       <SidebarMenuItem icon={<Upcoming />} title="Upcoming" />
       <SidebarMenuItem icon={<Task />} title="Completed" />
-      TODO: Load projects
+      <DividerWithPadding />
+      {projectList.map((project) => (
+        <SidebarMenuItem
+          key={project.id}
+          icon={<TaskAlt />}
+          title={project.name}
+        />
+      ))}
     </SidebarContainer>
   );
 };
---
+++ src/mockData/projectList.ts
@@ -0,0 +1,12 @@
+import { Project } from "../types/Project";
+
+export const projectList: Project[] = [
+  {
+    id: 1,
+    name: "Project 1",
+  },
+  {
+    id: 2,
+    name: "Project 2",
+  },
+] as const;
