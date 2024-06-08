68589cf7eb29511b9f1805165aaadef698da3128
Mitch Stark <MitchStark10@gmail.com>
2024-06-08 14:55:56 -0500

    prepare tags slice

---
+++ src/store/slices/tagSlice.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getAuthToken } from "../../lib/authToken";
+import { API_BASE_URL } from "../../lib/constants";
+import { Tag } from "../../types/Tag";
+
+export const tagsApi = createApi({
+  reducerPath: "tag",
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  tagTypes: ["Tags"],
+  endpoints: (builder) => ({
+    tags: builder.query<[], void>({
+      query: () => ({
+        url: "/tags",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      }),
+      providesTags: ["Tags"],
+    }),
+    createTag: builder.mutation<Tag, Omit<Tag, "id">>({
+      query: ({ tagName, tagColor }) => ({
+        url: "/tags",
+        method: "POST",
+        body: {
+          tagName,
+          tagColor,
+        },
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      }),
+      invalidatesTags: ["Tags"],
+    }),
+    updateTag: builder.mutation<Tag, Tag>({
+      query: ({ id, tagName, tagColor }) => ({
+        url: `/tags/${id}`,
+        method: "PUT",
+        body: {
+          tagName,
+          tagColor,
+        },
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      }),
+      invalidatesTags: ["Tags"],
+    }),
+    deleteList: builder.mutation<Tag, { id: string }>({
+      query: ({ id }) => ({
+        url: `/tags/${id}`,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      }),
+      invalidatesTags: ["Tags"],
+    }),
+  }),
+});
+
+export const {
+  useTagsQuery,
+  useCreateTagMutation,
+  useDeleteListMutation,
+  useUpdateTagMutation,
+} = tagsApi;
