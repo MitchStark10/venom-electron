6de6016296f379a360017c8a3aef0392d0af127e
Mitch Stark <mitchstark10@gmail.com>
2024-12-21 15:41:37 -0600

    support cmd key on mac

--- src/hooks/useGlobalShortcuts.ts
+++ src/hooks/useGlobalShortcuts.ts
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export const useGlobalShortcut = (shortcutKey: string, action: () => void) => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.ctrlKey && event.key === shortcutKey) {
+      if ((event.ctrlKey || event.metaKey) && event.key === shortcutKey) {
         console.log("processing");
         event.preventDefault();
         action();
