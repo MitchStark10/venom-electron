b29520cc6c40108c60fe48db05db470e90bbcd8f
Mitch Stark <mitchstark10@gmail.com>
2025-01-01 17:41:21 -0600

    fix weird safari issue

--- src/hooks/useNavigationShortcuts.ts
+++ src/hooks/useNavigationShortcuts.ts
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
+import { shallowEqual, useSelector } from "react-redux";
+import { RootState } from "../store/store";
 import { useGlobalShortcut } from "./useGlobalShortcuts";
 
 export const useNavigationShortcuts = () => {
+  const { focusView } = useSelector((state: RootState) => state.focusView);
+  const { selectedListId } = useSelector(
+    (state: RootState) => state.focusView,
+    shallowEqual
+  );
+
   const [focusedSection, setFocusedSection] = useState<
     "sidebar" | "focus-view"
   >("focus-view");
@@ -36,13 +44,23 @@ export const useNavigationShortcuts = () => {
   });
 
   useEffect(() => {
-    const focusableElement = document.querySelector(
+    const focusableElement: HTMLElement | null = document.querySelector(
       `.${focusedSection} [tabindex="${focusedIndex}"]`
     );
 
     if (focusableElement) {
-      (focusableElement as HTMLElement).focus();
+      // The contentEditable attribute is used to deal with a weird MUI + Safari issue
+      // where the focus() method doesn't consistently work
+      focusableElement.contentEditable = "true";
+      focusableElement.focus();
+      focusableElement.contentEditable = "false";
       focusableElement.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   }, [focusedIndex, focusedSection]);
+
+  useEffect(() => {
+    if (focusedSection === "focus-view") {
+      setFocusedIndex(-1);
+    }
+  }, [focusView, selectedListId, focusedSection]);
 };
