dddf24b8e143373423f6840534812b6e13e190bc
Mitch Stark <mitchstark10@gmail.com>
2025-01-05 16:25:09 -0600

    dynamic text

--- src/Layout/FocusView/StandupFocusView/index.tsx
+++ src/Layout/FocusView/StandupFocusView/index.tsx
@@ -2,16 +2,26 @@ import { CircularProgress } from "@mui/material";
 import { SectionDivider } from "../../../components/SectionDivider";
 import { Title } from "../../../components/Title";
 import { useStandupTasksQuery } from "../../../store/slices/taskSlice";
+import { useSettingsQuery } from "../../../store/slices/userSlice";
 import { TaskCard } from "../ListFocusView/TaskCard";
+
+const NoTasksToReport = () => <div>No tasks to report</div>;
 
 export const StandupFocusView = () => {
   const { data: standupTasks, isLoading } = useStandupTasksQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
+  const { data: settingsData } = useSettingsQuery();
 
   if (isLoading) {
     return <CircularProgress />;
   }
+
+  const isTodayMonday = new Date().getDay() === 1;
+  const yesterdayText =
+    isTodayMonday && settingsData?.dailyReportIgnoreWeekends
+      ? "Friday"
+      : "Yesterday";
 
   let index = 0;
 
@@ -19,17 +29,15 @@ export const StandupFocusView = () => {
     <div>
       <Title>Daily Report</Title>
       <div>
-        <SectionDivider>Yesterday</SectionDivider>
-        {standupTasks?.yesterday.length === 0 && (
-          <div>No tasks from yesterday</div>
-        )}
+        <SectionDivider>{yesterdayText}</SectionDivider>
+        {standupTasks?.yesterday.length === 0 && <NoTasksToReport />}
         {standupTasks?.yesterday.map((task) => (
           <TaskCard key={task.id} task={task} showListName index={index++} />
         ))}
       </div>
       <div>
         <SectionDivider>Today</SectionDivider>
-        {standupTasks?.today.length === 0 && <div>No tasks for today</div>}
+        {standupTasks?.today.length === 0 && <NoTasksToReport />}
         {standupTasks?.today.map((task) => (
           <TaskCard key={task.id} task={task} showListName index={index++} />
         ))}
